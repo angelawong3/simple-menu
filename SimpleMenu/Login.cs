dd678d621189da9c7f0102f7803d78defa7aa13d
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2023-01-08 17:48:01 +0000

    login page with logo done, tested

---
+++ SimpleMenu/Login.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SimpleMenu
+{
+    class Login
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Login";
+
+            MainMenu.writeLogo();
+
+            Console.WriteLine("Welcome Back!");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ SimpleMenu/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleMenu
+{
+	class MainMenu
+	{
+		public static void writeLogo()
+		{
+			string logo = @" __ _                 _                                
+/ _(_)_ __ ___  _ __ | | ___    /\/\   ___ _ __  _   _ 
+\ \| | '_ ` _ \| '_ \| |/ _ \  /    \ / _ \ '_ \| | | |
+_\ \ | | | | | | |_) | |  __/ / /\/\ \  __/ | | | |_| |
+\__/_|_| |_| |_| .__/|_|\___| \/    \/\___|_| |_|\__,_|
+               |_|
+";
+
+			Console.WriteLine(logo);
+
+        }
+	}
+}
+
